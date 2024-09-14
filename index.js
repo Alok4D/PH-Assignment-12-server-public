3c4f9a162a785121b3dfaf06d8d381f82a1655e5
Alok4D <alokroy602701@gmail.com>
2024-09-15 00:25:02 +0600

    add payment code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -29,7 +30,8 @@ async function run() {
     const announcementCartCollection = client.db("apartmentDB").collection("announcements");
     const apartmentCollection = client.db("apartmentDB").collection("apartmentData");
     const agreementCartCollection = client.db("apartmentDB").collection("agreementCarts");
-    
+    // 
+    const agreementViewCollection = client.db("apartmentDB").collection("agreementView");
 
     // user related api
     app.post("/users", async (req, res) => {
@@ -132,6 +134,56 @@ async function run() {
       const result = await agreementCartCollection.deleteOne(query);
       res.send(result);
     })
+
+   // get single room
+   app.get('/agreementDetails/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { menuId: id }
+    const result = await agreementCartCollection.findOne(query)
+    res.send(result)
+})
+
+// agreement view details server api //
+
+app.get('/agreementView', async(req, res) => {
+  const email = req.query.email;
+  const query = { email: email };
+  const result = await agreementViewCollection.find().toArray();
+  res.send(result);
+})
+
+app.post('/agreementView', async(req, res) => {
+  const agreementViewItem = req.body;
+  const result = await agreementViewCollection.insertOne(agreementViewItem)
+  res.send(result)
+})
+
+app.delete('/agreementView/:id', async(req, res) => {
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id) }
+  const result = await agreementViewCollection.deleteOne(query);
+  res.send(result);
+})
+
+//
+
+// payment intent system
+
+app.post('/create-payment-intent', async (req, res) => {
+  const {price} = req.body;
+  const amount = parseInt(price * 100);
+  console.log(amount, 'amount inside the intent!');
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: 'usd',
+    payment_method_types: ['card']
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  })
+})
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
